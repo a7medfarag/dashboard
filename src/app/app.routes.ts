7d44af97a4d2230595ef86f2d6075ceae0048212
a7medfarag <farga30@gmail.com>
2024-07-18 20:24:10 +0300

    add login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,32 +3,38 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 
 export const routes: Routes = [
-    {
-      path: '',
-      redirectTo: 'dashboard',
-      pathMatch: 'full',
-    },
-    {
-      path: 'sign-in',
-      loadChildren: () => import('./pages/sign-in/sign-in.component').then((m) => m.SignInComponent),
-    },
-    {
-        path: 'dashboard',
-		component: MainLayoutComponent,
-		children: [
-			{
-				path: '',
-				component: DashboardComponent,
-			}
-		],
-	},
-    {
-      path: 'not-found',
-      loadChildren: () => import('./pages/dashboard/dashboard.component').then((m) => m.DashboardComponent),
-    },
-  
-    {
-      path: '**',
-      redirectTo: 'not-found',
-    },
-  ];;
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'sign-in',
+    loadComponent: () =>
+      import('./pages/sign-in/sign-in.component').then(
+        (m) => m.SignInComponent
+      ),
+  },
+  {
+    path: 'dashboard',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent,
+      },
+    ],
+  },
+  {
+    path: 'not-found',
+    loadChildren: () =>
+      import('./pages/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent
+      ),
+  },
+
+  {
+    path: '**',
+    redirectTo: 'not-found',
+  },
+];
--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [AuthLayoutComponent],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
